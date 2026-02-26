2c4881dc3b7d21316ca619f0870759066e781012
Todoroscean Dragos <dragoshuniq@gmail.com>
2026-02-26 08:35:51 +0100

    Adjust format money in order to meet examples

--- src/features/home/utils/format-money.ts
+++ src/features/home/utils/format-money.ts
@@ -4,10 +4,12 @@ export function formatMoney(
   amount: number,
   currency: CurrencyCode = "USD"
 ): string {
+  const truncated = Math.floor(amount * 100) / 100;
+
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency,
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(amount);
+  }).format(truncated);
 }
